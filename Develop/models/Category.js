443e5bb19df9a8016ffbd8de0c53dd9d9436f3f4
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-06-03 00:24:52 -0500

    entered data for category.js

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -12,27 +12,13 @@ Category.init(
       primaryKey: true,
       autoIncrement: true
     },
-    comment_text: {
+    category_name: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
         len: [1]
       }
     },
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'user',
-        key: 'id'
-      }
-    },
-    post_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'post',
-        key: 'id'
-      }
-    }
   },
   {
     sequelize,
