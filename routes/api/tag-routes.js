26d52a2173243b745cabf0d4315a5df1f032f982
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-06-05 12:27:04 -0500

    adding template to tag-routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -26,3 +26,101 @@ router.delete('/:id', (req, res) => {
 });
 
 module.exports = router;
+
+
+
+
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+router.get('/', (req, res) => {
+
+  Category.findAll({
+    include: [Product]
+  })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
+});
+
+router.get('/:id', (req, res) => {
+  Category.findOne({
+    include: [Product],
+    where: {
+      id: req.params.id
+    }
+  })
+
+    .then(dbCategoryData => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'Category not found with this id!' });
+        return;
+      }
+
+      res.json(dbCategoryData);
+    })
+
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
+});
+
+router.post('/', (req, res) => {
+  Category.create({
+
+    category_name: req.body.category_name
+  })
+
+    .then((dbCategoryData) => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
+});
+
+router.put('/:id', (req, res) => {
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+
+    .then(dbCategoryData => {
+      if (!dbCategoryData[0]) {
+        res.status(404).json({ message: 'No category with this id!' });
+        return;
+      }
+    })
+
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
+});
+
+router.delete('/:id', (req, res) => {
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+
+    .then(dbCategoryData => {
+      if (!dbCategoryData) {
+        res.status(404).json({ message: 'No category found with this id!' });
+        return;
+      }
+      res.json(dbCategoryData);
+    })
+
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
+module.exports = router;
