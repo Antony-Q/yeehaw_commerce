6b12f0a2ccdcfe14fded5db2200805cec25aefe0
Antony Q <95669276+Antony-Q@users.noreply.github.com>
2022-06-05 14:13:36 -0500

    change null to true

--- models/Product.js
+++ models/Product.js
@@ -1,17 +1,14 @@
-// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
-// import our database connection from config.js
 const sequelize = require('../config/connection');
 
-// Initialize Product model (table) by extending off Sequelize's Model class
+
 class Product extends Model {}
 
-// set up fields and rules for Product model
 Product.init(
   {
     id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
       primaryKey: true,
       autoIncrement: true
     },
@@ -21,12 +18,12 @@ Product.init(
     },
     price: {
       type: DataTypes.DECIMAL(10, 2),
-      allowNull: false,
+      allowNull: true,
       validate: { isNumeric: true }
     },
     stock: {
       type: DataTypes.INTEGER,
-      allowNull: false,
+      allowNull: true,
       default: 10,
       validate: { isInt: true }
     },
